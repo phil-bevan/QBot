de0102acbc0fddc6669a498fc5c738f7931374ff
unknown <Phil Bevan@DESKTOP-COOQA8Q.ics-solutions.local>
2020-09-15 16:41:50 +0100

    Tweak typescript for qbotpbi deployment

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbotpbi.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbotpbi-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "4ea601d2-0c43-4e42-8e98-a346360a6a13",
+        clientId: "ca32e5e1-8007-4b62-aad6-78810ff7018a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
