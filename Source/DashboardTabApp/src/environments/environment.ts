26b0b104e360ea89b229558a46d54bbe7b9cf937
unknown <Phil Bevan@DESKTOP-COOQA8Q.ics-solutions.local>
2020-09-17 14:22:58 +0100

    testing

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "14d57f01-ed3c-44c1-ad8a-48174f1e8e62",
-        clientId: "8237bf74-f859-496f-9f85-d809a821f2f0",
+        clientId: "ec98b9b9-8418-47ce-bd91-c91d16fbe063",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
