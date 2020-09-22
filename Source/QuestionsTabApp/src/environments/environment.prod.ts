112faea0708da99485ce27090c74860f959f6f45
unknown <Phil Bevan@DESKTOP-COOQA8Q.ics-solutions.local>
2020-09-22 10:32:23 +0100

    qbotpaaa

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotpbbb-api.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotpbbb-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbotpaaa-api.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbotpaaa-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "232582d6-68db-4349-87a2-4e2512884066",
-        clientId: "9b282f31-4306-44e3-8aff-074e2b6331b0",
+        clientId: "87fe38b8-6395-4ab5-9fad-e4ac20f7c63f",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
