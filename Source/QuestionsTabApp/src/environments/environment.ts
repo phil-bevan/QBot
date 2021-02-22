b2d1a25ecff67454600c6cdb4f43aba8c105088a
phil-bevan <48096014+phil-bevan@users.noreply.github.com>
2021-02-22 12:49:12 +0000

    qbotdemo 12-48pm

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemo-api.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemo-questions.azurewebsites.net",
+    apiBaseUrl: "https://icsqbotdemo-api.azurewebsites.net/api/Request/",
+    selfUrl: "https://icsqbotdemo-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "01583be4-f096-49f4-bd19-56b649a8482f",
-        clientId: "115c7d88-1bf9-4df2-99ef-d3c415940e1a",
+        clientId: "096e806c-84e9-45d8-ab79-1f2405e7cdc7",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
